2e99a3ab8878f318ce1a4825be57eff3a73d4622
acwhale <csx5800@163.com>
2018-07-05 10:26:47 +0800

    排序

--- spider/spider.py
+++ spider/spider.py
@@ -8,7 +8,7 @@ class Spider():
     namePattern = '</i>([\s\S]*?)</span>'
     numberPattern = '<span class="video-number">([\s\S]*?)</span>'
 
-    #获取内容
+
     def __fetch_content(self):
         result = request.urlopen(Spider.url)
         htmls = result.read()
@@ -24,7 +24,7 @@ class Spider():
             anchor = {'name':name,'number':number}
             anchors.append(anchor)
         return anchors
-    #精炼数据
+
     def __refine(self,anchors):
         l = lambda anchor:{
             'name':anchor['name'][0].strip(),'number':anchor['number'][0]
@@ -32,11 +32,24 @@ class Spider():
         return map(l,anchors)
 
     def __sort(self,anchors):
-        pass
+        anchors = sorted(anchors,key=self.__sort_seed,reverse=True)
+        return anchors
+
+    def __sort_seed(self,anchor):
+        result = re.findall('\d*',anchor['number'])
+        number = float(result[0])
+        if '万' in anchor['number']:
+            number *= 10000
+        return number
+    def __show(self,anchors):
+        for rank in range(0,len(anchors)):
+            print('排序:' + str(rank+1) + ':' + anchors[rank]['name'] + "  "+anchors[rank]['number'])
     def go(self):
         htmls = self.__fetch_content()
         anchors = self.__analysis(htmls)
         anchors =list(self.__refine(anchors))
+        anchors = self.__sort(anchors)
+        self.__show(anchors)
 
 
 
